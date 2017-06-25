6cad14370e7aa184ade54973cc4b0c6343fcf84b
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2017-06-24 21:50:14 -0400

    Add lifecycle methods for the logging purposes

--- app/src/main/java/com/mnishiguchi/geoquiz/ui/MainActivity.kt
+++ app/src/main/java/com/mnishiguchi/geoquiz/ui/MainActivity.kt
@@ -67,4 +67,31 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, json)
         return QuestionBankJsonAdapter().fromJson(json)
     }
+
+    /* Lifecycle methods */
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy called")
+    }
 }
